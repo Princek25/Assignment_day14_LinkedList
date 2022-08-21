e412bad29cfa6eeb1a7f9ddd8eff9c8934b63346
Prince <princek2530@gmail.com>
2022-08-21 08:54:37 +0530

    Added: Insert 30 between 56 and 70

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -4,24 +4,9 @@ public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> list1 = new LinkedList<>();
         list1.push(70);
-        list1.push(30);
         list1.push(56);
-        list1.show();
+        list1.insert(30);
+        list1.display();
         System.out.println();
-
-        LinkedList<Integer> list2 = new LinkedList<>();
-        list2.add(56);
-        list2.add(30);
-        list2.add(70);
-        list2.display();
-        System.out.println();
-
-        LinkedList<Integer> list3 = new LinkedList<>();
-        list3.append(70);
-        list3.append(30);
-        list3.append(56);
-        list3.show();
-        System.out.println();
-
     }
 }
